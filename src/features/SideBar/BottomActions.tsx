964bb0525a0e24b53071102a30ef8bfd524b3538
Igroshka <86719527+Igroshka@users.noreply.github.com>
2023-12-18 01:06:50 +0700

    Update BottomActions.tsx

--- src/features/SideBar/BottomActions.tsx
+++ src/features/SideBar/BottomActions.tsx
@@ -1,22 +1,15 @@
-import { ActionIcon, DiscordIcon, Icon } from '@lobehub/ui';
-import { Badge, ConfigProvider, Dropdown, MenuProps } from 'antd';
+import { ActionIcon, Icon } from '@lobehub/ui';
+import { Dropdown, MenuProps } from 'antd';
 import {
-  Book,
-  Feather,
-  FileClock,
-  Github,
   HardDriveDownload,
   HardDriveUpload,
-  Heart,
   Settings,
   Settings2,
 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Flexbox } from 'react-layout-kit';
 
-import { ABOUT, CHANGELOG, DISCORD, FEEDBACK, GITHUB, WIKI } from '@/const/url';
 import DataImporter from '@/features/DataImporter';
 import { configService } from '@/services/config';
 import { GlobalStore, useGlobalStore } from '@/store/global';
@@ -30,13 +23,6 @@ export interface BottomActionProps {
 const BottomActions = memo<BottomActionProps>(({ tab, setTab }) => {
   const router = useRouter();
   const { t } = useTranslation('common');
-
-  const [hasNewVersion, useCheckLatestVersion] = useGlobalStore((s) => [
-    s.hasNewVersion,
-    s.useCheckLatestVersion,
-  ]);
-
-  useCheckLatestVersion();
 
   const items: MenuProps['items'] = [
     {
@@ -78,40 +64,8 @@ const BottomActions = memo<BottomActionProps>(({ tab, setTab }) => {
       type: 'divider',
     },
     {
-      icon: <Icon icon={Feather} />,
-      key: 'feedback',
-      label: t('feedback'),
-      onClick: () => window.open(FEEDBACK, '__blank'),
-    },
-    {
-      icon: <Icon icon={FileClock} />,
-      key: 'changelog',
-      label: t('changelog'),
-      onClick: () => window.open(CHANGELOG, '__blank'),
-    },
-    {
-      icon: <Icon icon={Book} />,
-      key: 'wiki',
-      label: 'WIKI',
-      onClick: () => window.open(WIKI, '__blank'),
-    },
-    {
-      icon: <Icon icon={Heart} />,
-      key: 'about',
-      label: t('about'),
-      onClick: () => window.open(ABOUT, '__blank'),
-    },
-    {
-      type: 'divider',
-    },
-    {
       icon: <Icon icon={Settings} />,
       key: 'setting',
-      label: (
-        <Flexbox align={'center'} distribution={'space-between'} gap={8} horizontal>
-          {t('setting')} {hasNewVersion && <Badge count={t('upgradeVersion.hasNew')} />}
-        </Flexbox>
-      ),
       onClick: () => {
         setTab(SidebarTabKey.Setting);
         useGlobalStore.setState({
@@ -124,33 +78,9 @@ const BottomActions = memo<BottomActionProps>(({ tab, setTab }) => {
   ];
 
   return (
-    <>
-      <ActionIcon
-        icon={DiscordIcon}
-        onClick={() => window.open(DISCORD, '__blank')}
-        placement={'right'}
-        title={'Discord'}
-      />
-      <ActionIcon
-        icon={Github}
-        onClick={() => window.open(GITHUB, '__blank')}
-        placement={'right'}
-        title={'GitHub'}
-      />
       <Dropdown arrow={false} menu={{ items }} trigger={['click']}>
-        {hasNewVersion ? (
-          <Flexbox>
-            <ConfigProvider theme={{ components: { Badge: { dotSize: 8 } } }}>
-              <Badge dot offset={[-4, 4]}>
-                <ActionIcon active={tab === SidebarTabKey.Setting} icon={Settings2} />
-              </Badge>
-            </ConfigProvider>
-          </Flexbox>
-        ) : (
           <ActionIcon active={tab === SidebarTabKey.Setting} icon={Settings2} />
-        )}
       </Dropdown>
-    </>
   );
 });
 

999a149f59ba0137698d8c2f83212d6c423a3cf1
Igroshka <86719527+Igroshka@users.noreply.github.com>
2023-12-18 01:08:30 +0700

    Update Inner.tsx

--- src/app/market/features/ShareAgentButton/Inner.tsx
+++ src/app/market/features/ShareAgentButton/Inner.tsx
@@ -1,12 +1,11 @@
 import { Icon } from '@lobehub/ui';
-import { Button, Divider, Tag } from 'antd';
-import { Github, Settings, Share2 } from 'lucide-react';
+import { Tag } from 'antd';
+import { Settings, Share2 } from 'lucide-react';
 import Image from 'next/image';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import MobilePadding from '@/components/MobilePadding';
-import { AGENTS_INDEX_GITHUB } from '@/const/url';
 
 const Inner = memo(() => {
   const { t } = useTranslation('market');
@@ -32,20 +31,6 @@ const Inner = memo(() => {
         {' - '}
         {t('guide.func1.desc2')}
       </p>
-      <Divider />
-      <h3>
-        <Tag color={'cyan'}>{t('guide.func2.tag')}</Tag>
-        <span>{t('guide.func2.title')}</span>
-      </h3>
-      <p>{t('guide.func2.desc')}</p>
-      <br />
-      <Button
-        icon={<Icon icon={Github} />}
-        onClick={() => window.open(AGENTS_INDEX_GITHUB, '__blank')}
-        type={'primary'}
-      >
-        {t('guide.func2.button')}
-      </Button>
     </MobilePadding>
   );
 });
